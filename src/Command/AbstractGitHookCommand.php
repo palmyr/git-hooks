7368cba137f9f6801890fcde70cefb5e22477ff3
Ryan Shane Palm <rspalm@bigpond.com>
2022-05-18 09:23:29 +0200

    Added new command

---
+++ src/Command/AbstractGitHookCommand.php
@@ -0,0 +1,10 @@
+<?php declare(strict_types=1);
+
+namespace Palmyr\GitHooks\Command;
+
+use Symfony\Component\Console\Command\Command;
+
+abstract class AbstractGitHookCommand extends Command
+{
+
+}
