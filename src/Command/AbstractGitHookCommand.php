cdf83940ac13568483256419b4c07c8c914fb20d
Ryan Shane Palm <rspalm@bigpond.com>
2022-05-22 19:18:02 +0200

    Added log on start of command

--- src/Command/AbstractGitHookCommand.php
+++ src/Command/AbstractGitHookCommand.php
@@ -5,6 +5,8 @@ namespace Palmyr\GitHooks\Command;
 use Psr\Log\LoggerInterface;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
 
 abstract class AbstractGitHookCommand extends Command
 {
@@ -25,5 +27,16 @@ abstract class AbstractGitHookCommand extends Command
         parent::configure();
 
         $this->addArgument('branch', InputArgument::REQUIRED, 'The current branch');
+    }
+
+    protected function initialize(InputInterface $input, OutputInterface $output)
+    {
+        parent::initialize($input, $output);
+
+        $this->logger->info(sprintf(
+            "Executing hook \"%s\" on branch \"%s\"",
+            $input->getArgument("command"),
+            $input->getArgument("branch")
+        ));
     }
 }
