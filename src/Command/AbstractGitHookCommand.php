d4c927b1d5f7076dc296981208ae852fc059db2b
Ryan Shane Palm <rspalm@bigpond.com>
2022-05-22 19:07:19 +0200

    Test on hook

--- src/Command/AbstractGitHookCommand.php
+++ src/Command/AbstractGitHookCommand.php
@@ -2,9 +2,28 @@
 
 namespace Palmyr\GitHooks\Command;
 
+use Psr\Log\LoggerInterface;
 use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
 
 abstract class AbstractGitHookCommand extends Command
 {
 
+    protected LoggerInterface $logger;
+
+    public function __construct(
+        LoggerInterface $logger,
+        string $name
+    )
+    {
+        $this->logger = $logger;
+        parent::__construct('git:' . $name);
+    }
+
+    protected function configure()
+    {
+        parent::configure();
+
+        $this->addArgument('branch', InputArgument::REQUIRED, 'The current branch');
+    }
 }
